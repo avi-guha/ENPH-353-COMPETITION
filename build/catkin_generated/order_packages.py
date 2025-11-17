37d458f3ecf8851480f272c858c95ae13851db23
fizzer <emajkic@student.ubc.ca>
2025-11-17 14:34:10 -0800

    Time trials package - finished implementation

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/fizzer/ENPH-353-COMPETITION/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/fizzer/enph353_ws/devel;/opt/ros/noetic'.split(';') if '/home/fizzer/enph353_ws/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,6 +8,9 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/fizzer/ENPH-353-COMPETITION/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/fizzer/ENPH-353-COMPETITION/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/fizzer/ENPH-353-COMPETITION/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/fizzer/ENPH-353-COMPETITION/build'
-export ROSLISP_PACKAGE_DIRECTORIES="/home/fizzer/ENPH-353-COMPETITION/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export PYTHONPATH="/home/fizzer/ENPH-353-COMPETITION/devel/lib/python3/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/fizzer/ENPH-353-COMPETITION/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/fizzer/ENPH-353-COMPETITION/src:$ROS_PACKAGE_PATH"
