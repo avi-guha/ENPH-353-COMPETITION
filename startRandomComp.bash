979b619fe3ef1317ab179d724106fd970735af37
avi-guha <avi.guha05@gmail.com>
2025-11-30 21:22:55 -0800

    Add start scripts from clue-board-reading

--- startRandomComp.bash
+++ startRandomComp.bash
@@ -2,7 +2,7 @@ cd /home/fizzer/ENPH-353-COMPETITION
 source devel/setup.bash
 
 cd src/enph353/enph353_utils/scripts
-./run_sim.sh -vpgw &
+./run_sim_random.sh -vpgw &
 sleep 3
 
 cd /home/fizzer/ENPH-353-COMPETITION
