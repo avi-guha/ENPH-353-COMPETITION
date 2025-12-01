0fc7c310bbc90311575dbee10656bcdd41a596ae
fizzer <emajkic@student.ubc.ca>
2025-11-30 21:18:13 -0800

    Start files

--- startComp.bash
+++ startComp.bash
@@ -1,3 +1,13 @@
+cd /home/fizzer/ENPH-353-COMPETITION
 source devel/setup.bash
+
 cd src/enph353/enph353_utils/scripts
-./run_sim.sh -vpgw
+./run_sim.sh -vpgw &
+sleep 3
+
+cd /home/fizzer/ENPH-353-COMPETITION
+source devel/setup.bash
+
+cd src/enph353/enph353_utils/scripts
+./score_tracker.py
+
--- startRandomComp.bash
+++ startRandomComp.bash
@@ -1,5 +1,13 @@
+cd /home/fizzer/ENPH-353-COMPETITION
 source devel/setup.bash
-cd src/enph353/enph353_utils/scripts
-./run_sim_random.sh -gw
 
+cd src/enph353/enph353_utils/scripts
+./run_sim_random.sh -vpgw &
+sleep 3
+
+cd /home/fizzer/ENPH-353-COMPETITION
+source devel/setup.bash
+
+cd src/enph353/enph353_utils/scripts
+./score_tracker.py
 
